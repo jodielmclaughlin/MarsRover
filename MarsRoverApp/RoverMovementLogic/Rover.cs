4939bc0b47dd03b38a118396332cab73a4e66247
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-04-14 14:09:24 +0100

    RoverMovementLogic and LogicTests folders added. Rotate method added in new Rover class and Tests in RoverTests completed

---
+++ MarsRoverApp/RoverMovementLogic/Rover.cs
@@ -0,0 +1,61 @@
+﻿using MarsRoverApp.RoverMovement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverApp.RoverMovementLogic
+{
+    public class Rover
+    {
+        public Position Position { get; set; }
+        public string Name { get; set; }
+
+        public Rover(Position position, string name)
+        {
+            Position = position;
+            Name = name;
+        }
+
+        public Compass Rotate(Rover rover, Instruction instruction) //TODO: Refactor when time
+        {
+            if (instruction == Instruction.M)
+            { 
+                throw new ArgumentException("Instruction M (Move) is not valid for rotation"); 
+            }
+
+            if (instruction == Instruction.R)
+            {
+                if (rover.Position.Facing == Compass.N)
+                {
+                    return Compass.E;
+                }
+                if (rover.Position.Facing == Compass.E)
+                {
+                    return Compass.S;
+                }
+                if (rover.Position.Facing == Compass.S)
+                {
+                    return Compass.W;
+                }
+            }
+            else if(instruction == Instruction.L)
+            {
+                if (rover.Position.Facing == Compass.N)
+                {
+                    return Compass.W;
+                }
+                if (rover.Position.Facing == Compass.W)
+                {
+                    return Compass.S;
+                }
+                if (rover.Position.Facing == Compass.S)
+                {
+                    return Compass.E;
+                }
+            }
+            return Compass.N;
+        }
+    }
+}
